bd90b4856d4b14a263e081001ea50f08dae62bbf
VladDev322 <VladL322@yandex.ru>
2023-06-14 04:40:24 +0300

    lesson 7

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,9 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-  use HasFactory;
   public function products()
   {
-    return $this->belongsToMany(Product::class);
+    return $this->belongsToMany(Product::class)->withPivot('count')->withTimestamps();
+  }
+  
+  public function getFullPrice()
+  {
+    $sum = 0;
+    foreach ($this->products as $product) {
+      $sum += $product->getPriceForCount();
+    }
+    return $sum;
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', [MainController::class, 'index'])->name('index');
 Route::get('/basket', [BasketController::class, 'basket'])->name('basket');
 Route::get('/basket/place', [BasketController::class, 'basketPlace'])->name('basket-place');
 Route::post('/basket/add/{id}', [BasketController::class, 'basketAdd'])->name('basket-add');
+Route::post('/basket/remove/{id}', [BasketController::class, 'basketRemove'])->name('basket-remove');
 Route::get('/categories', [MainController::class, 'categories'])->name('categories');
-Route::get('/{category}/{product?}', [MainController::class, 'product'])->name('product');
 Route::get('/{category}', [MainController::class, 'category'])->name('category');
+Route::get('/{category}/{product?}', [MainController::class, 'product'])->name('product');
