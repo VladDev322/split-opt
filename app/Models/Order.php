1349e51ecee17947baea48178befe2cd108a010d
VladDev322 <VladL322@yandex.ru>
2023-06-14 19:34:15 +0300

    lesson 8

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,7 +11,7 @@ class Order extends Model
   {
     return $this->belongsToMany(Product::class)->withPivot('count')->withTimestamps();
   }
-  
+
   public function getFullPrice()
   {
     $sum = 0;
@@ -19,5 +19,19 @@ class Order extends Model
       $sum += $product->getPriceForCount();
     }
     return $sum;
+  }
+
+  public function saveOrder($name, $phone)
+  {
+    if ($this->status == 0) {
+      $this->name = $name;
+      $this->phone = $phone;
+      $this->status = 1;
+      $this->save();
+      session()->forget('orderId');
+      return true;
+    } else {
+      return false;
+    }
   }
 }
