84ab5d350b642267ce93b2e2c38124449e506753
VladDev322 <VladL322@yandex.ru>
2023-06-21 07:44:48 +0300

    lesson 18

--- resources/views/auth/categories/index.blade.php
+++ resources/views/auth/categories/index.blade.php
@@ -42,6 +42,7 @@
       @endforeach
     </tbody>
   </table>
+  {{$categories->links('pagination::bootstrap-4')}}
   <a class="btn btn-success" type="button" href="{{ route('categories.create') }}">Добавить категорию</a>
 </div>
 @endsection
